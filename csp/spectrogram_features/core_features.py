8ee9d0cbec63ac624bc43bc2dcf3d814f692cb51
Konstantinos Drosos <kostas.drosos@icloud.com>
2020-04-18 12:16:03 +0300

    Fixed empty lines and spaces.

--- csp/spectrogram_features/core_features.py
+++ csp/spectrogram_features/core_features.py
@@ -8,9 +8,7 @@ import librosa
 import matplotlib.pyplot as plt
 
 
-
 # get the spectrogram features
-
 class Spectrogram_features(object):
     
     def __init__(self, audio_path):
@@ -18,17 +16,13 @@ class Spectrogram_features(object):
         self.audio_path = audio_path
         self.EPS        = 1e-8
         self.start      = int(np.random.uniform(-4800,4800))
-        
-    
-    
+
     @staticmethod
     def wav_to_signal(audio_path):
         
         signal, source     = librosa.load(audio_path,sr=None)
         return {'signal' : signal , 'source' : source }
-    
-    
-    
+
     @staticmethod
     def get_spectrogram(signal, window = 'hamming'):
         
@@ -40,38 +34,30 @@ class Spectrogram_features(object):
         magnitude, phase = librosa.magphase(fourier_transform)
         
         return {'magnitude' : magnitude, 'phase' : phase }
-        
-    
-    
+
     def spectrogram_data(self):
-        
-        
         """Separate a complex-valued spectrogram 
             D into its magnitude (S) 
             and phase (P) components, 
             so that D = S * P."""
         
-        self.data   = Spectrogram_features.wav_to_signal(self.audio_path)
+        self.data = Spectrogram_features.wav_to_signal(self.audio_path)
         self.signal = self.data['signal']
-        self.sr     = self.data['source']
+        self.sr = self.data['source']
         
-        self.spec  = Spectrogram_features.get_spectrogram(self.signal, window = 'hamming')
-        magnitude  = self.spec['magnitude']
+        self.spec = Spectrogram_features.get_spectrogram(self.signal, window='hamming')
+        magnitude = self.spec['magnitude']
         
-        log_spectrogram  = np.log(magnitude)
+        log_spectrogram = np.log(magnitude)
         
         plt.imshow(log_spectrogram, aspect='auto', origin='lower',)
         plt.title('Raw audio spectrogram')
-        
-        
-        
-        return {
-                'magnitude'           : magnitude,
-                'signal'              : self.signal,
-                'shape'               : self.signal.shape, 
-                'max'                 : self.signal.max(), 
-                'min'                 : self.signal.min() ,
-                'log_spectrogram'     : log_spectrogram ,
-                'sr'                  : self.sr,
-                'plt'                 : plt
-                }
+
+        return {'magnitude': magnitude,
+                'signal': self.signal,
+                'shape': self.signal.shape,
+                'max': self.signal.max(),
+                'min': self.signal.min(),
+                'log_spectrogram': log_spectrogram,
+                'sr': self.sr,
+                'plt': plt}
