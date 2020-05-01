9d57081bd83a1cad7bee9875692096cef7e822c4
Aaditya Ura <aadityaura@gmail.com>
2020-05-01 05:52:43 +0530

    Update time_domain.py

--- csp/audio_features/time_domain.py
+++ csp/audio_features/time_domain.py
@@ -329,3 +329,46 @@ def signal_entropy(signal):
         ent -= i * math.log(i, math.exp(1))
 
     return ent
+
+ def waveletTransform(lagu):
+    cA, cD = pywt.dwt(lagu, 'db1')
+    return cD
+
+def signalCorrelate(fullLagu, potonganLagu):
+    signalCorrelate = signal.correlate(fullLagu, potonganLagu, mode='full')
+    return signalCorrelate
+
+def crossCorrelation(fullLagu, potonganLagu):
+    hasilCross = np.correlate(fullLagu, potonganLagu)
+    return hasilCross
+
+def magnitude(crossCorrelation):
+    magnitude = np.sqrt(crossCorrelation.dot(crossCorrelation))
+    return magnitude
+
+# wavelenght denoising of time signals
+def denoise_wvlt( x, wavelet, level):
+    """
+
+    1. Adapted from waveletSmooth function found here:
+
+    http://connor-johnson.com/2016/01/24/using-pywavelets-to-remove-high-frequency-noise/
+
+    2. Threshold equation and using hard mode in threshold as mentioned
+
+    by Tomas Vantuch:
+
+    http://dspace.vsb.cz/bitstream/handle/10084/133114/VAN431_FEI_P1807_1801V001_2018.pdf
+
+    """
+    # Decompose to get the wavelet coefficients
+    coeff = pywt.wavedec( x, wavelet, mode="per" )
+    # Calculate sigma for threshold as defined in http://dspace.vsb.cz/bitstream/handle/10084/133114/VAN431_FEI_P1807_1801V001_2018.pdf
+    # As noted by @harshit92 MAD referred to in the paper is Mean Absolute Deviation not Median Absolute Deviation
+
+    sigma = (1/0.6745) * maddest( coeff[-level] )
+    # Calculte the univeral threshold
+    uthresh = sigma * np.sqrt( 2*np.log( len( x ) ) )
+    coeff[1:] = ( pywt.threshold( i, value=uthresh, mode='hard') for i in coeff[1:] )  
+    # Reconstruct the signal using the thresholded coefficients
+    return pywt.waverec( coeff, wavelet, mode='per' )
