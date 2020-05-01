fded9ebaa2a8035e767aca76614d5d3f4e1e7ed3
Aaditya Ura <aadityaura@gmail.com>
2020-05-02 03:44:54 +0530

    Update time_domain.py

--- csp/audio_features/time_domain.py
+++ csp/audio_features/time_domain.py
@@ -108,7 +108,7 @@ def get_loudness(sound, slice_size=60*1000):
 def cough_signal_segmentation(signal, min_silence_len, silence_thresh):
     
     # decide min_silence_len from all cough_audio
-    cough_s = split_on_silence (signal, 
+    cough_s = silence.split_on_silence (signal, 
                                 min_silence_len = min_silence_len, 
                                 silence_thresh = signal.dBFS-silence_thresh
                                )
@@ -199,14 +199,20 @@ def formant_frequencies(signal, total_formats, order_type, sample_freq, formant_
 
     """
     # https://www.sciencedirect.com/science/article/abs/pii/S0167639301000498
-    signal_order_ = {'gautam_method' : formant_value + 2, 'other_method' : int(2 + sample_freq/1000) }
+    
+    all_frequencies = []
+    signal_order_ = {'gautam_method' : formant_value + 2, 'other_method' : int(2 + sample_freq/1000)}
 
-    A = librosa.core.lpc(y, signal_order_[order_type])
-    rts = np.roots(A)
-    rts = rts[np.imag(rts) >= 0]
-    angz = np.arctan2(np.imag(rts), np.real(rts))
-    frqs = angz * sample_freq / (2 *  np.pi)
-    return frqs.sort()
+    for sub_frame in signal:
+        
+        A = librosa.core.lpc(sub_frame, signal_order_[order_type])
+        rts = np.roots(A)
+        rts = rts[np.imag(rts) >= 0]
+        angz = np.arctan2(np.imag(rts), np.real(rts))
+        frqs = sorted(angz * sample_freq / (2 *  math.pi))
+        all_frequencies.append(frqs[:total_formats])
+     
+    return all_frequencies
         
 
                         
@@ -257,7 +263,7 @@ def melfrequency_cepstral_coefficients(mfcc_type,
                 }
     
     else:
-        mfcc = spe_feats.mfcc(signal , 
+        mfcc = spf.mfcc(signal , 
                               sample_freq , 
                               winlen  = frame_size, 
                               winstep = frame_size, 
@@ -265,11 +271,11 @@ def melfrequency_cepstral_coefficients(mfcc_type,
                               winfunc = winfunc
                              )
         
-        mfcc_delta  = spe_feats.delta(mfcc,1)
-        mfcc_delta2 = spe_feats.delta(mfcc_delta_feat,1)
+        mfcc_delta  = spf.delta(mfcc,1)
+        mfcc_delta2 = spf.delta(mfcc_delta,1)
         
         return {
-                'mfcc' : mfccs, 
+                'mfcc' : mfcc, 
                 'mfcc_delta' : mfcc_delta, 
                 'mfcc_delta2': mfcc_delta2
                 }
@@ -278,16 +284,20 @@ def melfrequency_cepstral_coefficients(mfcc_type,
 # sig=ACfU3U1MUEJ-J9fdBizOC3HXky-IeYcH1A&hl=en&sa=X&ved=2ahUKEwiA5Obg95DpAhVcCTQIHVywDy0Q6AEwC3oECAsQAQ#v=onepage&
 # q=zero%20crossing%20rate%20divided%20by%20length%20minus%201&f=false
 def compute_zcr(signal):
-    
+
     def change_sign(v1, v2):
         return v1 * v2 < 0
-
-    s = 0
-    for ind, _ in enumerate(signal):
-        if ind + 1 < len(signal):
-            if change_sign(signal[ind], signal[ind+1]):
-                s += 1
-    return s/(len(signal)-1)  # return zcr for each frame
+    
+    all_frame_values = []
+    for frame in signal:
+        s = 0
+        for ind, _ in enumerate(frame):
+            if ind + 1 < len(frame):
+                if change_sign(frame[ind], frame[ind+1]):
+                    s += 1
+        all_frame_values.append(s/(len(frame)-1))
+                    
+    return all_frame_values  # return zcr for each frame
 
 
  
@@ -379,10 +389,14 @@ def denoise_wvlt( x, wavelet, level):
   
 def skew_and_kurtosis(signal_frame):
     
-    return {
-            'skew' : skew(signal_frame), 
-            'kurtosis' : kurtosis(signal_frame)
-            }
+    all_values = {'skew' :  [], 'kurtosis' : [] }
+    
+    for subframe in signal_frame:
+        
+        all_values['skew'].append(skew(subframe))
+        all_values['kurtosis'].append(kurtosis(subframe))
+        
+    return all_values
  
 
 
@@ -410,7 +424,11 @@ def parabolic(f, x):
     yv = f[x] - 1/4 * (f[x-1] - f[x+1]) * (xv - x)
     return (xv, yv)
 
-
+#Obtain autocorrelation
+def autocorr(x):
+    result = np.correlate(x, x, mode='full')
+    return result[int((result.size+1)/2):] #Note: other people use re.size/2:, but this does not work for me 
+                                   # TODO: check consistency in other computers
 
 def freq_from_autocorr(sig, fs):
     """
@@ -432,3 +450,104 @@ def freq_from_autocorr(sig, fs):
     px, py = parabolic(corr, peak)
 
     return fs / px
+
+
+def indexes(y, thres=0.3, min_dist=1, thres_abs=False):
+    """Peak detection routine borrowed from 
+    https://bitbucket.org/lucashnegri/peakutils/src/master/peakutils/peak.py
+    """
+    if isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.unsignedinteger):
+        raise ValueError("y must be signed")
+
+    if not thres_abs:
+        thres = thres * (np.max(y) - np.min(y)) + np.min(y)
+
+    min_dist = int(min_dist)
+
+    # compute first order difference
+    dy = np.diff(y)
+
+    # propagate left and right values successively to fill all plateau pixels (0-value)
+    zeros, = np.where(dy == 0)
+
+    # check if the signal is totally flat
+    if len(zeros) == len(y) - 1:
+        return np.array([])
+
+    if len(zeros):
+        # compute first order difference of zero indexes
+        zeros_diff = np.diff(zeros)
+        # check when zeros are not chained together
+        zeros_diff_not_one, = np.add(np.where(zeros_diff != 1), 1)
+        # make an array of the chained zero indexes
+        zero_plateaus = np.split(zeros, zeros_diff_not_one)
+
+        # fix if leftmost value in dy is zero
+        if zero_plateaus[0][0] == 0:
+            dy[zero_plateaus[0]] = dy[zero_plateaus[0][-1] + 1]
+            zero_plateaus.pop(0)
+
+        # fix if rightmost value of dy is zero
+        if len(zero_plateaus) and zero_plateaus[-1][-1] == len(dy) - 1:
+            dy[zero_plateaus[-1]] = dy[zero_plateaus[-1][0] - 1]
+            zero_plateaus.pop(-1)
+
+        # for each chain of zero indexes
+        for plateau in zero_plateaus:
+            median = np.median(plateau)
+            # set leftmost values to leftmost non zero values
+            dy[plateau[plateau < median]] = dy[plateau[0] - 1]
+            # set rightmost and middle values to rightmost non zero values
+            dy[plateau[plateau >= median]] = dy[plateau[-1] + 1]
+
+    # find the peaks by using the first order difference
+    peaks = np.where(
+        (np.hstack([dy, 0.0]) < 0.0)
+        & (np.hstack([0.0, dy]) > 0.0)
+        & (np.greater(y, thres))
+    )[0]
+
+    # handle multiple peaks, respecting the minimum distance
+    if peaks.size > 1 and min_dist > 1:
+        highest = peaks[np.argsort(y[peaks])][::-1]
+        rem = np.ones(y.size, dtype=bool)
+        rem[peaks] = False
+
+        for peak in highest:
+            if not rem[peak]:
+                sl = slice(max(0, peak - min_dist), peak + min_dist + 1)
+                rem[sl] = True
+                rem[peak] = False
+
+        peaks = np.arange(y.size)[~rem]
+
+    return peaks
+
+
+
+
+def freq_from_autocorr_improved(signal, fs):
+    signal -= np.mean(signal)  # Remove DC offset
+    corr = fftconvolve(signal, signal[::-1], mode='full')
+    corr = corr[len(corr)//2:]
+
+    # Find the first peak on the left
+    i_peak = indexes(corr, thres=0.8, min_dist=5)[0]
+    i_interp = parabolic(corr, i_peak)[0]
+
+    return fs / i_interp, corr, i_interp
+
+
+def logenergy_computation(signal):
+    
+    #https://dsp.stackexchange.com/questions/17829/the-logarithm-energy-measure-in-speech
+    
+    total_energy = []
+    for sub_frame in signal:
+        constant     = 2e-22
+        log_base     = np.log10
+        signal_power = sub_frame**2
+        # flatten      = np.array(sub_frame).flatten()
+        sum_all      = reduce(lambda x, y: (x + y), signal_power)/len(sub_frame) + constant
+        total_energy.append(log_base(sum_all))
+    return total_energy
